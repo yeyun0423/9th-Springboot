1ba2230b45d055098ffd3812880cf99b03f7e6ec
최예윤 <pet2ytty@naver.com>
2025-09-19 03:34:46 +0900

    Feat:스프링부트 초기 환경설정

---
+++ src/main/java/com/example/umc_9th_springboot/Umc9thSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc_9th_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc9thSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc9thSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/umc_9th_springboot/Umc9thSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc_9th_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc9thSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
